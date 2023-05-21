7cf671f0a8d939d2c60ba41ea4b08ad1b3c7504b
Camron <cawe219@gmail.com>
2023-05-20 20:17:26 -0400

    initializing

---
+++ src/main/java/dev/Camron/App/Authentication/AppAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package dev.Camron.App.Authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAuthenticationApplication.class, args);
+	}
+
+}
