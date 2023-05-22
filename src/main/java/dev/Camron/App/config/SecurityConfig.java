263944de90051d5afd3af50560d5c3a2718fe203
Camron <Cawe219@gmail.com>
2023-05-21 21:51:03 -0400

    base securty configuration.

---
+++ src/main/java/dev/Camron/App/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package dev.Camron.App.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig{
+    
+}
