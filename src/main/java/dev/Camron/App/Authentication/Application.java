562233846f3ac6bbbc3008c3dad713a3df22de7f
Camron West <Cawe219@gmail.com>
2023-07-12 19:04:43 -0400

    Update Application.java

--- src/main/java/dev/Camron/App/Authentication/Application.java
+++ src/main/java/dev/Camron/App/Authentication/Application.java
@@ -2,7 +2,6 @@ package dev.Camron.App.Authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
